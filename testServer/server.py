f5fbdde025e699d29bbb4d843ec4a1b17f838dc2
Andrew Higgins <Andrew.R.Higgins@colorado.edu>
2021-07-06 04:27:52 -0600

    3

---
+++ testServer/server.py
@@ -0,0 +1,56 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from ssl import SSLContext, PROTOCOL_TLS_SERVER
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+ip = '127.0.0.1'
+port = 8443
+context = SSLContext(PROTOCOL_TLS_SERVER)
+context.load_cert_chain('cert.pem', 'key.pem')
+
+
+class requestHandler(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+        str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+
+with socket(AF_INET, SOCK_STREAM) as server:
+    server.bind((ip, port))
+    server.listen(1)
+    with context.wrap_socket(server, server_side=True) as tls:
+        connection, address = tls.accept()
+        print(f'Connected by {address}\n')
+
+        data = connection.recv(1024)
+        print(f'Client Says: {data}')
+        connection.sendall(b"You're welcome")
+
+        server_class=HTTPServer
+        handler_class=requestHandler
+        logging.basicConfig(level=logging.INFO)
+        server_address = ('', port)
+        httpd = server_class(server_address, handler_class)
+        logging.info('Starting httpd...\n')
+
+        try:
+            httpd.server_forever()
+        except KeyboardInterrupt:
+            pass
+        logging.info('Stopping httpd...\n')
+        httpd.server_close()
